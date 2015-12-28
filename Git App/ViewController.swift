f352e89661aa8cc8d7e9dac00386749b3e1ff3d3
Brandon Bart <brandonbart@me.com>
2015-12-28 15:11:46 -0500

    Initial Commit

---
+++ Git App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Git App
+//
+//  Created by Brandon Bart on 12/28/15.
+//  Copyright © 2015 Bart. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
